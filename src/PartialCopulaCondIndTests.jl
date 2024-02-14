383494046f1c460a25203bb8d41386a51b77ca8d
Lasse Petersen <lassepetersen@protonmail.com>
2024-02-14 10:08:23 +0100

    added main package object

--- src/PartialCopulaCondIndTests.jl
+++ src/PartialCopulaCondIndTests.jl
@@ -1,5 +1,6 @@
 module PartialCopulaCondIndTests
 
+include("conditional_independence_test.jl")
 include("generalized_correlation.jl")
 
 end
---
+++ src/conditional_independence_test.jl
@@ -0,0 +1,11 @@
+using StatsAPI: HypothesisTest
+
+abstract type ConditionalIndependenceTest <: HypothesisTest end
+abstract type IndependenceTest <: HypothesisTest end
+abstract type PartialCopulaEstimator end
+
+
+struct PartialCopulaCondIndTest <: ConditionalIndependenceTest
+    partial_copula_estimator::PartialCopulaEstimator
+    independence_test::IndependenceTest
+end
