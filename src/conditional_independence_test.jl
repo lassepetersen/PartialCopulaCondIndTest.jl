be574946b9b331c79bcc9a77accfdcb77502df8c
Lasse Petersen <lassepetersen@protonmail.com>
2024-03-18 11:11:36 +0100

    moving some code around and defining abstract types in main module

--- src/conditional_independence_test.jl
+++
@@ -1,9 +0,0 @@
-abstract type ConditionalIndependenceTest <: HypothesisTest end
-abstract type IndependenceTest <: HypothesisTest end
-abstract type PartialCopulaEstimator end
-
-
-struct PartialCopulaCondIndTest <: ConditionalIndependenceTest
-    partial_copula_estimator::PartialCopulaEstimator
-    independence_test::IndependenceTest
-end
